f094de226d2f21177e6bfc56c8cee02de7479389
Constant Guilbert <constant@debian.home>
2022-04-27 15:26:13 +0200

    Separating Tailwind styles from vanilla CSS stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,22 +3,22 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="{{URL::asset('css/app.css')}}" rel="stylesheet">
+	<link href="css/site.css" rel="stylesheet">
 	<!-- Le titre de la page -->
 	<title>Poulets hyper populaires</title>
-	
-	
+
+
 </head>
 <body>
 <header>
             <div id="titre_principal">
                 <div id="logo">
                     <img src="" />
-            	<h1>Poulets hyper populaires</h1>    
+            	<h1>Poulets hyper populaires</h1>
                 </div>
                 <h2>Les meilleurs selfies de poulets</h2>
             </div>
-            
+
             <nav>
                 <ul>
                     <li><a href="/register">Inscription</a></li>
@@ -30,4 +30,27 @@
 	@yield("content")
 
 </body>
+
+<footer>
+                <div id="tweet">
+                    <h1>Microblogging</h1>
+                    <p>!</p>
+                    <p>ca vous a ...</p>
+                </div>
+                <div id="mes_photos">
+                    <h1>Exemples de PHP</h1>
+                    <p><img src="" alt="Poulet 1" /><img src="images/photo2.jpg" alt="Photographie" /><img src="images/photo3.jpg" alt="Photographie" /><img src="images/photo4.jpg" alt="Photographie" /></p>
+                </div>
+                <div id="mes_amis">
+                    <h1>Nos informations</h1>
+                    <div id="listes_amis">
+                        <ul>
+                            <li><a href="#">Constant</a></li>
+                            <li><a href="#">Celine</a></li>
+                            <li><a href="#">Esther</a></li>
+                            <li><a href="#">Sonia/a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
 </html>
